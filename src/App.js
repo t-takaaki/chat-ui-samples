7aececd84cdd753285205009b9ee1e99b4e57c26
t-takaaki <oden.tanaka38@gmail.com>
2021-09-18 17:34:30 +0900

    remove comments

--- src/App.js
+++ src/App.js
@@ -11,10 +11,6 @@ const App = () => {
           <nav style={{ padding: "20px 0", backgroundColor: "#F2F2F2", marginBottom: "20px"}}>
             <NavLink activeClassName="active" to="tailwind" style={{color: "blue", marginRight: "20px", display: "inline-block"}}>tailwind chat</NavLink>
             <NavLink activeClassName="active" to="elements" style={{color: "blue", display: "inline-box"}}>react chat elements</NavLink>
-            {/*
-            <a href="/tailwind">tailwind</a>
-            <a href="/elements">elements</a>
-             */}
           </nav>
         </header>
         <Switch>
